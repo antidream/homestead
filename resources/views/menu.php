76f8965648cc7e82a1e4953f470ab58b1ee3ba80
AntiDream <ikozakov96@yandex.ru>
2020-08-07 15:29:13 +0300

    laravel created

---
+++ resources/views/menu.php
@@ -0,0 +1,3 @@
+<a href="/">Главная</a><br>
+<a href="/about">О нас</a><br>
+<a href="/news">Новости</a>
---
+++ resources/views/news.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php include "menu.php" ?>
+<div><h3>Отказ российских артистов выступать в Белоруссии</h3></div><h3>
+</body>
+</html>
+
